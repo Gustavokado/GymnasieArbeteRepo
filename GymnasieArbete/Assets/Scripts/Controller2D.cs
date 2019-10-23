0e170ebf59e05aeb553c1013cc2d257d61ae670a
Gustavokado <54946763+Gustavokado@users.noreply.github.com>
2019-10-23 13:45:35 +0200

    23/10
    första delen av spelar kontrollen

--- GymnasieArbete/Assets/Scripts/Controller2D.cs
+++ GymnasieArbete/Assets/Scripts/Controller2D.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Controller2D : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const float skinWidth = .015f;
+
+    BoxCollider2D collider;
+    RaycastOrigins raycasyOrigins;
+    
     void Start()
     {
-        
+        collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateRaycastOrigins()
     {
-        
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycasyOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycasyOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycasyOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycasyOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
     }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+
+  
 }
