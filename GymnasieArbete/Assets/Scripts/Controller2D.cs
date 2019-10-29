6ecfe88ecefac3a2680b529fc43387818dc10ce3
Gustavokado <gusmel79@gmail.com>
2019-10-29 17:13:11 +0100

    episode 3 done

--- GymnasieArbete/Assets/Scripts/Controller2D.cs
+++ GymnasieArbete/Assets/Scripts/Controller2D.cs
@@ -15,6 +15,7 @@ public class Controller2D : MonoBehaviour
 
     BoxCollider2D collider;
     RaycastOrigins raycastOrigins;
+    public CollisionInfo collisions;
 
     void Start()
     {
@@ -25,6 +26,7 @@ public class Controller2D : MonoBehaviour
     public void Move(Vector3 velocity)
     {
         UpdateRaycastOrigins();
+        collisions.Reset();
         if (velocity.x != 0)
         {
             HorizontalCollisions(ref velocity);
@@ -54,6 +56,9 @@ public class Controller2D : MonoBehaviour
             {
                 velocity.x = (hit.distance - skinWidth) * directionX;
                 rayLength = hit.distance;
+
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
             }
         }
     }
@@ -75,6 +80,9 @@ public class Controller2D : MonoBehaviour
             {
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
             }
         }
     }
@@ -106,6 +114,18 @@ public class Controller2D : MonoBehaviour
     {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
     }
 
 }
