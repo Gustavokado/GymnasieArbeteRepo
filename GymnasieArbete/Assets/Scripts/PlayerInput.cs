4f40ef599cb648b19b5b45349aa7d56f118ef6d8
Gustavokado <gusmel79@gmail.com>
2019-11-23 21:51:36 +0100

    controller

--- GymnasieArbete/Assets/Scripts/PlayerInput.cs
+++ GymnasieArbete/Assets/Scripts/PlayerInput.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     Player player;
-   
+    float movementSpeed = 2.0f;
+
     void Start()
     {
         player = GetComponent<Player>();
@@ -15,22 +16,39 @@ public class PlayerInput : MonoBehaviour
   
     void Update()
     {
+        /*float rTriggerFloat = Input.GetAxis("Right Trigger");
+        float lTriggerFloat = Input.GetAxis("Left Trigger");
+        bool leftBumper = Input.GetButton("Left Bumper");
+        bool rightBumper = Input.GetButton("Right Bumper");
+        bool backButton = Input.GetButton("Back");
+        bool startButton = Input.GetButton("Start");
+        bool aButton = Input.GetButton("A Button");
+        bool bButton = Input.GetButton("B Button");
+        bool xButton = Input.GetButton("X Button");
+        bool yButton = Input.GetButton("Y Button");
+        float dpadHorizontal = Input.GetAxis("Dpad Horizontal") * movementSpeed;
+        float dpadVertical = Input.GetAxis("Dpad Vertical") * movementSpeed;
+        float moveH = Input.GetAxis("Horizontal") * movementSpeed;
+        float moveV = Input.GetAxis("Vertical") * movementSpeed;
+        float moveHR = Input.GetAxis("Mouse X") * movementSpeed;
+        float moveVR = Input.GetAxis("Mouse Y") * movementSpeed;*/
+
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.SetDirectionalInput(directionalInput);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetButtonDown("A Button"))
         {
             player.OnJumpInputDown();
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetButtonUp("A Button"))
         {
             player.OnJumpInputUp();
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetButtonDown("B Button"))
         {
             player.OnSprintInputDown();
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetButtonUp("B Button"))
         {
             player.OnSprintInputUp();
         }
